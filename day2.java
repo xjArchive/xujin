8161938eca29f518c6645927866838f8a28f9b74
xjArchive <1697084799@qq.com>
2019-05-03 23:53:44 +0800

    提交1

---
+++ day2.java
@@ -0,0 +1,12 @@
+class  test01
+{
+	public static void main(String[] args) 
+	{	for (int m=1;m<5;m++)
+		{
+		   for(int i=1;i<4;i++)
+			{
+		            System.out.println("Hello World!");
+			}
+		}
+	}
+}
